05c50e785ad1c44a2447177efb55e6f76081b68a
Achmad Xander L.E.J <achmadxander@student.ittelkom-sby.ac.id>
2022-03-12 15:59:58 +0700

    Initial Commit

---
+++ db_swalayan_edo.php
@@ -0,0 +1,8 @@
+<?php 
+	$hostname = 'localhost';
+	$username = 'root';
+ 	$password = '';
+ 	$dbname   = 'db_swalayan_edo';
+
+ 	$conn = mysqli_connect($hostname,$username,$password,$dbname) or die ('Gagal terhubung ke database');
+ ?>
