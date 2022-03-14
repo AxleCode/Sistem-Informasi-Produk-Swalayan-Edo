e03144642b5a999635743aa60aba43bc887bb56d
Achmad Xander L.E.J <achmadxander@student.ittelkom-sby.ac.id>
2022-03-14 19:55:35 +0700

    Halaman Login Oke

---
+++ dashboard.php
@@ -0,0 +1,52 @@
+<?php 
+//agar jika admin langsung membuka web dashboard, maka akan diarahkan ke halaman login dulu
+	session_start();
+	if ($_SESSION['status_login'] != true) {
+		echo '<script>window.location="login.php"</script>';
+	}
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard | Swalayan Edo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"> 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head> 
+<body>
+	<!-- Bagian Header -->
+	<header>
+		<div class="container">
+		<h1><a href="dashboard.php">Swalayan Edo</a></h1>
+		<ul>
+			<li><a href="dashboard.php">Dashboard</a></li>
+			<li><a href="profil.php">Profil</a></li>
+			<li><a href="data-kategori.php">Data Kategori</a></li>
+			<li><a href="data-produk.php">Data Produk</a></li>
+			<li><a href="logout.php">keluar</a></li>
+		</ul>
+	</div>
+	</header>
+
+	<!-- Bagian Content -->
+	<div class="section">
+		<div class="container">
+			<h3>Dashboard</h3>
+			<div class="box">
+				<h4>Selamat Datang <?php echo $_SESSION['para_admin']->admin_nama?></h4>
+			</div>
+		</div>
+	</div>
+
+	<!-- Bagian Footer -->
+	<footer>
+		<div class="container">
+			<small>By DARK STREGHT TEAM</small>
+		</div>
+	</footer>
+</body>
+</html>
--- login.php
+++ login.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login | Swalayan Edo</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css"> 
+	<!--baris 9-11 link code font ambil digoogle-->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&family=Quicksand&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
@@ -18,12 +19,24 @@
 			<input type="submit" name="submit" value="Login" class="tombollogin">
 		</form>
 		<?php 
+
 			if (isset($_POST['submit'])) {
+				session_start();
 				include 'db_swalayan_edo.php';
 				$user = $_POST['user'];
 				$pass = $_POST['pass'];
-
-				$cek = mysqli_query($conn, "SELECT + FROM tabel_admin WHERE username = '".$user."' AND password = '".md5($pass)."'");
+				
+				$cek = mysqli_query($conn, "SELECT * FROM tabel_admin WHERE username = '".$user."' AND password = '".md5($pass)."'");
+				//mencocokkan apabila user dan password sama dengan yang ada didatabase maka akan keluar angka 1 jika tidak ada maka 0
+				if( mysqli_num_rows($cek) > 0){
+					$d = mysqli_fetch_object($cek);
+					$_SESSION['status_login'] = true;
+					$_SESSION['para_admin'] = $d;
+					$_SESSION['id'] = $d->admin_id;
+					echo '<script>window.location="dashboard.php"</script>';
+				}else{
+					echo '<script>alert("Username dan Password Salah!")</script>';
+				}
 			}
 		 ?>
 	</div>	
