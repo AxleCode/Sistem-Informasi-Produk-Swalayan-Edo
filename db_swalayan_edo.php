3df1277bf555aa63a58bd4c8cad6e9e6955b5b95
AxleCode <93900125+AxleCode@users.noreply.github.com>
2022-03-26 10:54:28 +0700

    Update db_swalayan_edo.php

--- db_swalayan_edo.php
+++ db_swalayan_edo.php
@@ -1,8 +1,14 @@
-<?php 
-	$hostname = 'localhost';
-	$username = 'root';
- 	$password = '';
- 	$dbname   = 'db_swalayan_edo';
+<?php
+$servername = "localhost";
+$database = "id18675903_db_swalayan_edo";
+$username = "id18675903_swalayan_edo";
+$password = "Darkstreght_1";
 
- 	$conn = mysqli_connect($hostname,$username,$password,$dbname) or die ('Gagal terhubung ke database');
- ?>
+// untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
+// membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $database);
+// mengecek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
